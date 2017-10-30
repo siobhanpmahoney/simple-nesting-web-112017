b0daac9c3af9f2057300afbc00aa0ced88437bd5
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-10-30 19:37:48 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -51,7 +51,7 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => ["C"][0]
         }
      }
      programmer_hash[:dennis_ritchie][:languages]
